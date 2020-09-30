dc1bd61298dca70d4c6185a96a601e4e8415d230
Chan Chun Hin <chan@owndays.co.jp>
2020-10-01 00:38:22 +0900

    First Init

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    public Text btnText;
+
+
+    // Unity Object
+    public GameObject MainMenu;
+    public GameObject QuestionPanel;
+    public Text filePathText;
+    public Text questionNumberText;
+    public Toggle hideQuestionToggle;
+    public Toggle shuffleToggle;
+    public Toggle reviewOnlyToggle;
+
+
+    private List<RowData> RowDataList { get; set; }
+    private int CurrentRowNumber { get; set; }
+    public string FilePath { get; set; }
+    public bool IsShuffleChecked { get; set; }
+    public bool IsReviewOnlyChcked { get; set; }
+    public bool IsQuestionHidden { get; set; }
+    public int QuestionNumber { get; set; } = 1;
+
+
+    // Functions
+
+
+    public void OnStartButtonClick()
+    {
+        // 開始パネルを非表示
+        MainMenu.active = false;
+
+        // 指定ディレクトリ（EXEと同じPath）にある.xlsxを取得
+        string[] filePaths = Directory.GetFiles(Application.dataPath, "*.xlsx");
+
+        if (filePaths.Length == 1)
+        {
+            // xlsxパスを変数に保存する
+            FilePath = filePaths[0];
+
+            // パスラベルを更新
+            filePathText.text = $"ファイルパス: {FilePath}";
+            btnText.text = FilePath;
+
+            // 問題一覧の取得
+            RowDataList = LoadData.LoadDataFromExcel(FilePath, false, false);
+
+            // 問題番号リセットする
+            QuestionNumber = 1;
+            questionNumberText.text = $"問題:{QuestionNumber}/{RowDataList.Count()}";
+        }
+        else if (filePaths.Length > 1)
+        {
+            //　複数エクセを見つかった場合
+            // MessageBox.Show("エラー: 複数エクセルが見つかりました。",
+            //                 "姉御に連絡して！",
+            //                 MessageBoxButtons.OK,
+            //                 MessageBoxIcon.Exclamation);
+
+            // 強制終了
+            // Application.Exit();
+        }
+        else if (filePaths.Length <= 0)
+        {
+            //　エクセを見つからなかった場合
+            // MessageBox.Show("エラー: エクセルがありません。",
+            //                 "姉御に連絡して！",
+            //                 MessageBoxButtons.OK,
+            //                 MessageBoxIcon.Exclamation);
+
+            // 強制終了
+            // Application.Exit();
+        }
+
+        // chkShuffleにチェック入ったら、問題一覧をシャッフルする
+        if (IsShuffleChecked)
+        {
+            RowDataList = RowDataList.OrderBy(row => Guid.NewGuid()).ToList();
+        }
+        else
+        {
+            RowDataList = RowDataList.OrderBy(row => row.RowNumber).ToList();
+        }
+
+        // ロードした問題のID
+        CurrentRowNumber = RowDataList.First().RowNumber;
+
+        // 一番目の問題をtbxQuestionにロードする
+        // tbxQuestion.Text = RowDataList.First().Question;
+
+        // 一番目の印をchkShouldReviewにロードする
+        // chkShouldReview.Checked = Convert.ToBoolean(RowDataList.First().ShouldReview);
+
+        // tbxAnswerを初期化
+        // tbxAnswer.Text = String.Empty;
+    }
+}
---
+++ Assets/Scripts/LoadData.cs
@@ -0,0 +1,161 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class LoadData
+{
+    /// <summary>
+    /// エクセルファイルからデータを読み込み
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public static List<RowData> LoadDataFromExcel(string filePath, bool isReviewOnlyChcked, bool IsQuestionHidden)
+    {
+        try
+        {
+            // パスのエクセルを読み込む
+            var workbook = new XLWorkbook(filePath);
+            // 1行目はヘッダーの想定で、それをスキップして2列目かつデータを入ってるセルのみ読み込む。
+            var rows = workbook.Worksheet(1).RangeUsed().RowsUsed().Skip(1);
+
+            // エラーチェック
+            if (!rows.Any())
+            {
+                //　Nullチェック
+                // MessageBox.Show("エラー: エクセルファイルにデータがありません。",
+                //                 "姉御に連絡して！ ",
+                //                 MessageBoxButtons.OK,
+                //                 MessageBoxIcon.Exclamation);
+
+                // Application.Exit();
+            }
+            //else
+            //{
+
+            //// フォマードチェック
+            //MessageBox.Show("姉御に連絡して！ エクセルのフォマードが対応していません。",
+            //                "残念でした",
+            //                MessageBoxButtons.OK,
+            //                MessageBoxIcon.Exclamation);
+
+            //Environment.Exit(1);
+            //}
+
+            // RowDataのリストを作る
+            var rowDataList = new List<RowData>();
+
+            // エラーリスt
+            var errorList = new List<int>();
+
+            // rowsのデータをRowDataに突っ込む
+            foreach (var row in rows)
+            {
+                var rowData = new RowData();
+
+                // TODO 数式も読めるように書き直す
+                int rowNumber = row.RowNumber();
+                string id = row.Cell("A").GetString();
+                string question = row.Cell("B").GetString();
+                string answer = row.Cell("C").GetString();
+                string shouldReview = row.Cell("D").GetString();
+
+                // 空セルチェック
+                if (string.IsNullOrEmpty(id) ||
+                   string.IsNullOrEmpty(question) ||
+                   string.IsNullOrEmpty(answer) ||
+                   string.IsNullOrEmpty(shouldReview))
+                {
+                    errorList.Add(rowNumber);
+                }
+
+                rowData.RowNumber = rowNumber;
+                if (Int32.TryParse(id, out int idInt))
+                {
+                    rowData.Id = idInt;
+                }
+                else
+                {
+                    // MessageBox.Show($"通番は数字のみ　行: {rowNumber}",
+                    //                 "姉御に連絡して！",
+                    //                 MessageBoxButtons.OK,
+                    //                 MessageBoxIcon.Exclamation);
+                    // 強制終了
+                    // Application.Exit();
+                }
+
+                // TODO 綺麗に書く。　無理やりフラグによってデータを入れ替えて返してる
+                if (IsQuestionHidden)
+                {
+                    rowData.Question = answer;
+                    rowData.Answer = question;
+                }
+                else
+                {
+                    rowData.Question = question;
+                    rowData.Answer = answer;
+                }
+
+                if (Int32.TryParse(shouldReview, out int shouldReviewInt))
+                {
+                    rowData.ShouldReview = shouldReviewInt;
+                }
+                else
+                {
+                    // MessageBox.Show($"フラグ（印）は0か1のみ　行: {rowNumber}",
+                    //                 "姉御に連絡して！",
+                    //                 MessageBoxButtons.OK,
+                    //                 MessageBoxIcon.Exclamation);
+                    // 強制終了
+                    // Application.Exit();
+                }
+
+                // 突っ込んだRowDataをリストに突っ込む
+                rowDataList.Add(rowData);
+            }
+
+            // エラーリストにエラーがあったら、メッセージ出して強制終了
+            if (errorList.Any())
+            {
+                // MessageBox.Show("空セル - 行: " + String.Join(", ", errorList),
+                //                 "姉御に連絡して！",
+                //                 MessageBoxButtons.OK,
+                //                 MessageBoxIcon.Exclamation);
+                // 強制終了
+                // Application.Exit();
+            }
+
+            // 印の問題のみにフィルタリング
+            if (isReviewOnlyChcked)
+            {
+                rowDataList = rowDataList.Where(row => row.ShouldReview == 1).ToList();
+            }
+
+
+            return rowDataList;
+        }
+        catch (Exception ex)
+        {
+            // MessageBox.Show("エラー: " + ex.Message,
+            //                 "姉御に連絡して！",
+            //                 MessageBoxButtons.OK,
+            //                 MessageBoxIcon.Exclamation);
+            // 強制終了
+            // Application.Exit();
+
+            throw ex;
+        }
+    }
+}
+
+/// <summary>
+/// エクセルから読み込んだモデル
+/// </summary>
+public class RowData
+{
+    public int RowNumber { get; set; }
+    public int Id { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public int ShouldReview { get; set; }
+}
